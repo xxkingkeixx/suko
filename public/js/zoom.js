bdc58d4643f65932b89738f1e6a67f42c68b90ba
solicited <mitchanimugen@gmail.com>
2016-04-30 04:15:05 +0000

    lol

--- public/js/zoom.js
+++ public/js/zoom.js
@@ -1,7 +1,6 @@
 
-            function zoom() {
-            document.body.style.webkitTransform =         // Chrome, Opera, Safari
-  document.body.style.msTransform =           // IE 9
-  document.body.style.transform = 'scale(.75)';
+        function zoom() {
+            document.body.style.zoom = "75%" 
         }
+
 
